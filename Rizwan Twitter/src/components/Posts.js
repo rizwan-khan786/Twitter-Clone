bf4c869a68b86c339ccb474124519b6fe8f1b7d0
Rizwan Islam khan <86212958+rizwan-khan786@users.noreply.github.com>
2021-09-13 13:18:58 +0530

    Add files via upload

---
+++ Rizwan Twitter/src/components/Posts.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  FaRegCheckCircle,
+  FaComment,
+  FaRegChartBar,
+  FaHeart,
+  FaLeaf,
+} from "react-icons/fa";
+const Posts = () => {
+  return (
+    <div className="postss">
+      <div className="postss__first">
+        <div className="posts__first__img">
+          <img src="/images/pic-1.png" alt="profile img" />
+        </div>
+        <div className="posts__first__name">
+          <strong>Rizwan Khan</strong> <FaRegCheckCircle className="verify" />
+        </div>
+        <div className="posts__first__username">
+          @rizwanikhan92 <span>6m</span>
+        </div>
+      </div>
+      <div className="postss__details">
+        <div className="postss__details__msg">
+         So Sweet she is ?
+        </div>
+        <div className="postss__details__img">
+          <img src="/images/pic-2.png" alt="post" />
+        </div>
+        <div className="reactions">
+          <span>
+            <FaComment className="re" /> 45
+          </span>
+          <span>
+            <FaRegChartBar className="re" /> 4
+          </span>
+          <span>
+            <FaHeart className="re" /> 345
+          </span>
+          <span>
+            <FaLeaf className="re" /> 234
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
